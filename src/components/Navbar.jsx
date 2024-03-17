7edd792c5e4dbcb0f645f8162d61f3f464c54201
unknown <shambelmighty51@gmail.com>
2024-03-17 22:17:00 +0100

    data

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 function Navbar() {
   return (
-    <nav className="p-2 pl-6 shadow-md top-0 sticky bg-white ">
+    <nav className="p-2 pl-6 shadow-md top-0 sticky bg-white text-red ">
       <img class="w-16 h-16 max-w-full h-auto " src="I000G0.jpg" />
     </nav>
   );
